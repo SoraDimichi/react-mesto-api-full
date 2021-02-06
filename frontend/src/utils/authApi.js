57426edbe04b2cca73dd22b1e3409193dfcf5a25
sorad <soradimichi@gmail.com>
2021-02-06 22:53:53 +0300

    fix: url update

--- frontend/src/utils/authApi.js
+++ frontend/src/utils/authApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.soradimichi.students.nomoredomains.rocks';
 
 const fetchData = (baseURL, path, params) => fetch(`${baseURL}${path}`, params).then((res) => {
   if (res.ok) {
